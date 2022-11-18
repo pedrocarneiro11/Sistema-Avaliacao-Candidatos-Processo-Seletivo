df256c9e0f16e148f59f73f03304cc68d9ce29ba
Pedro <pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.com>
2022-11-18 10:57:22 -0300

    Arredondando e formatando os valores dos salarios

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,11 +1,36 @@
 package candidatura;
 
+import java.text.DecimalFormat;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        analisarCandidato(1900.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
+        selecaoCandidatos();
     }
+
+    static void selecaoCandidatos() {
+        String[] candidatos = {"FELIPE","MARCIA","JULIA","PAULO","AUGUSTO","MONICA","FABRICIO","MIRELA","DANIELA","JORGE"};
+
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+        DecimalFormat df = new DecimalFormat("###.##"); // Arredondar o valor para duas casas decimais
+        while(candidatosSelecionados<5 && candidatoAtual < candidatos.length) {
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " solicitou um salario de R$ " + df.format(salarioPretendido));
+            if(salarioBase >= salarioPretendido) {
+                System.out.println("O candidato "+ candidato +" foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatoAtual++;
+        }
+    }
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800,2200);
+    }
+
     static void analisarCandidato(double salarioPretendido) {
         double salarioBase = 2000.00;
         if(salarioBase>salarioPretendido) {
