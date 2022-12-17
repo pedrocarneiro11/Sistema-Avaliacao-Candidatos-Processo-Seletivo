9ced547fbaee547ef8ee6ba5564e10813cd83b18
Pedro <pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.com>
2022-12-17 12:01:03 -0300

    colocando o metodo main em um novo arquivo Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import candidatura.ProcessoSeletivo;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ProcessoSeletivo processoSeletivo = new ProcessoSeletivo();
+        String[] candidatos = {"FELIPE","MARCIA","JULIA","PAULO","AUGUSTO"};
+        processoSeletivo.imprimirSelecionados();
+
+        for (String candidato: candidatos) {
+            processoSeletivo.entrandoEmContato(candidato);
+        }
     }
 }
--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -6,14 +6,7 @@ import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
-    public static void main(String[] args) {
-        String[] candidatos = {"FELIPE","MARCIA","JULIA","PAULO","AUGUSTO"};
-        imprimirSelecionados();
-        for (String candidato: candidatos) {
-            entrandoEmContato(candidato);
-        }
-    }
-    static void entrandoEmContato(String candidato) {
+    public static void entrandoEmContato(String candidato) {
         int tentativasRealizadas = 1;
         boolean continuarTentando = true;
         boolean atendeu = false;
@@ -38,7 +31,7 @@ public class ProcessoSeletivo {
         // ira retornar "true", se retornar "true" o candidato tera atendido
     }
 
-    static void imprimirSelecionados() {
+    public static void imprimirSelecionados() {
         String[] candidatos = {"FELIPE","MARCIA","JULIA","PAULO","AUGUSTO"};
         System.out.println("Imprimindo a lista de candidatos informando o indice do elemento: ");
         for(int indice = 0; indice < candidatos.length; indice++) {
